2104337f8b835804d0d22add1cbbfb623a0cf774
JuliaOraz <97658697+JuliaOraz@users.noreply.github.com>
2022-09-26 23:23:00 +0300

    Update api-images.jsx

--- src/api/api-images.jsx
+++ src/api/api-images.jsx
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
 const API_KEY = "29177947-674c6129c973bef5742ba95fc";
-const BASE_URL = "//pixabay.com/api";
+const BASE_URL = "https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientation=horizontal";
 const instance = axios.create({
   baseUrl: BASE_URL,
   params: {
-    key: API_KEY,
-    image_type: 'photo',
-    orientation: 'horizontal',
     per_page: 12,
   },
 });
